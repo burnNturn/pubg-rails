c3d26c7821c87e9a19d92cd68c0645f5ea038559
Cory Nistler <c.nistler3@yahoo.com>
2018-04-12 20:49:09 +0000

    got discord to work (don't ask how)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '158309731849-ah2svhnb9ggsimhsnvsdkekkmank862o.apps.googleusercontent.com', '7vGi92WnE4nmIzaK7-waVBfH', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  byebug
   provider :discord, '433079804770779146', 'Ow12Vm4o8IDsNNDQDNkYAXSe5qli6uhj'
 end
