a15ea2bb30e35191be27fb3bf5211e71ff1f568f
Cory Nistler <c.nistler3@yahoo.com>
2018-04-15 01:53:03 +0000

    started adding fields to the show page, need to make them hidden

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks =>
     "users/omniauth_callbacks", :registrations => "registrations" }
     
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
