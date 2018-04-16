dcf037d542fecc7aec802bcce7af87309ff567e9
Jarrod <jarrodmerryman@gmail.com>
2018-04-16 05:23:52 +0000

    adds helper functions to make content more user friendly

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -16,6 +16,15 @@ MATCH_SEARCH_PARAMS = {
     }
 }
 
+MATCH_TYPE = {
+    'solo-fpp': "Solo - First Person",
+    'solo-tpp': "Solo - Third Person",
+    'duo-fpp': "Duo - First Person",
+    'due-tpp': "Dup - Third Person",
+    'squad-fpp': "Squad - First Person",
+    'squad-tpp': "Squad - Third Person"
+}
+
 module StatsHelper
     def self.find_participant(rosters, ign)
         rosters.each do |roster|
@@ -33,5 +42,14 @@ module StatsHelper
             options_string << "<option>#{MATCH_SEARCH_PARAMS[opts[:key].to_sym][key]}</option>"
         end
         options_string
+    end
+    
+    def self.friendly_duration_time(duration)
+        (duration / 60).to_i.to_s + ":" + (duration % 60).to_s
+    end
+    
+    def self.friendly_match_type(type)
+        return MATCH_TYPE[type.to_sym] if MATCH_TYPE[type.to_sym]
+        "Event Game Mode (#{type})"
     end
 end
