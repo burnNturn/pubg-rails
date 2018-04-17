8fcbdea51b2d8dc287d4cfab1aa08ac317dbbcea
Cory Nistler <c.nistler3@yahoo.com>
2018-04-17 23:12:50 +0000

    switched back to edit through devise extended controller instead of user controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,14 +9,39 @@ class RegistrationsController < Devise::RegistrationsController
     # add custom create logic here
   end
   
-  def shown
+  def show
     
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    respond_to do |format|
+      if @user.update!(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :edit, status: :ok, location: @user }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
   end
 
   protected
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+  
+  private
+  
+  def user_params
+    byebug
+    params.require(:user).permit(:name, :email, :pubg_account)
   end
   
   
